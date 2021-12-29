353027115039c800f714d96771372d5bdf4fc83a
Vladyslav Tuzenko <karten.vlad@gmail.com>
2021-12-29 13:16:41 +0200

    Small cosmetic upgrade

--- client/src/components/WatchList/WatchList.jsx
+++ client/src/components/WatchList/WatchList.jsx
@@ -5,23 +5,23 @@ export const WatchList = ({ watchList, stocksList, deleteTicker, addTicker }) =>
   return (
     <div className="WatchList">
       <div className="WatchList__parts">
-        <h2>Your watch list: </h2>
+        <h2 className="WatchList__title">Your watch list: </h2>
         <ul className="WatchList__list">
           {watchList.map(ticker => (
           <li key={ticker} className="WatchList__item">
             <span>{ticker}</span>
-            <button onClick={() => deleteTicker(ticker)}>x</button>
+            <button className="WatchList__item-btn" onClick={() => deleteTicker(ticker)}>x</button>
           </li>
         ))}
         </ul>
       </div>
       <div className="WatchList__parts">
-        <h2>All available stocks: </h2>
+        <h2 className="WatchList__title">All available stocks: </h2>
         <ul className="WatchList__list">
           {stocksList.map(ticker => (
             <li key={ticker} className="WatchList__item">
               <span>{ticker}</span>
-              <button onClick={() => addTicker(ticker)}>+</button>
+              <button className="WatchList__item-btn" onClick={() => addTicker(ticker)}>+</button>
             </li>
           ))}
         </ul>
